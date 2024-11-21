4420bed4a38bd560c3a3833a5bedeb58bc85ceec
tcirtsa <tcirtsa_mail@163.com>
2024-11-21 14:16:09 +0800

    修改增加like的方法

--- back side/code/src/handler/like.rs
+++ back side/code/src/handler/like.rs
@@ -2,7 +2,7 @@ use crate::connect;
 use crate::db::schema::likes::dsl::*;
 use crate::model::like_model::Like;
 
-use crate::handler::post::{*,self};
+use crate::handler::post::{self, *};
 
 use diesel::prelude::*;
 use salvo::{prelude::*, Error};
@@ -16,8 +16,13 @@ pub async fn get_like_count(req: &mut Request, res: &mut Response) -> Result<(),
 #[handler]
 pub async fn add_like(req: &mut Request, res: &mut Response) -> Result<(), Error> {
     let mut conn = connect().unwrap();
-    let mut new_like = req.parse_json::<Like>().await?;
-    new_like.like_date = Some(chrono::Utc::now().naive_utc() + chrono::Duration::hours(8));
+    let p_id = req.param::<i32>("post_id").unwrap();
+    let u_id = req.param::<i32>("user_id").unwrap();
+    let new_like = Like {
+        post_id: p_id,
+        user_id: u_id,
+        like_date: Some(chrono::Utc::now().naive_utc() + chrono::Duration::hours(8)),
+    };
     let result = diesel::insert_into(likes)
         .values(&new_like)
         .execute(&mut conn);
